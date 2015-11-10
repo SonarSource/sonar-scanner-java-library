b20efcfeb2360afccb2abb7aaac4a57917ac5cbb
Julien HENRY <julien.henry@sonarsource.com>
2015-11-10 15:13:40 +0100

    SQSCANNER-1 Extract SonarQube Scanner as a standalone project

--- travis.sh
+++ travis.sh
@@ -4,45 +4,36 @@ set -euo pipefail
 
 function installTravisTools {
   mkdir ~/.local
-  curl -sSL https://github.com/SonarSource/travis-utils/tarball/v16 | tar zx --strip-components 1 -C ~/.local
+  curl -sSL https://github.com/SonarSource/travis-utils/tarball/v21 | tar zx --strip-components 1 -C ~/.local
   source ~/.local/bin/install
 }
 
-case "$TESTS" in
+if [ -n "${PR_ANALYSIS:-}" ] && [ "${PR_ANALYSIS}" == true ]
+then
+  if [ "$TRAVIS_PULL_REQUEST" != "false" ]
+  then
+    # For security reasons environment variables are not available on the pull requests
+    # coming from outside repositories
+    # http://docs.travis-ci.com/user/pull-requests/#Security-Restrictions-when-testing-Pull-Requests
+    if [ -n "$SONAR_GITHUB_OAUTH" ]; then
 
-CI)
-  installTravisTools
+      # Switch to java 8 as the Dory HTTPS certificate is not supported by Java 7
+      export JAVA_HOME=/usr/lib/jvm/java-8-oracle
+      export PATH=$JAVA_HOME/bin:$PATH
 
+      # PR analysis
+      mvn verify sonar:sonar -B -e -V \
+        -Dsonar.analysis.mode=issues \
+        -Dsonar.github.pullRequest=$TRAVIS_PULL_REQUEST \
+        -Dsonar.github.repository=$TRAVIS_REPO_SLUG \
+        -Dsonar.github.login=$SONAR_GITHUB_LOGIN \
+        -Dsonar.github.oauth=$SONAR_GITHUB_OAUTH \
+        -Dsonar.host.url=$SONAR_HOST_URL \
+        -Dsonar.login=$SONAR_LOGIN \
+        -Dsonar.password=$SONAR_PASSWD  
+    fi
+  fi
+else
+  # Regular CI
   mvn verify -B -e -V
-  ;;
-
-IT-DEV)
-  installTravisTools
-
-  build_snapshot "SonarSource/sonarqube" 
-
-  mvn install -Dsource.skip=true -Denforcer.skip=true -Danimal.sniffer.skip=true -Dmaven.test.skip=true
-
-  cd it
-  mvn -DsonarRunner.version="2.5-SNAPSHOT" -Dsonar.runtimeVersion="DEV" -Dmaven.test.redirectTestOutputToFile=false install
-  ;;
-
-IT-DEV-SQ51)
-  installTravisTools
-
-  mvn install -Dsource.skip=true -Denforcer.skip=true -Danimal.sniffer.skip=true -Dmaven.test.skip=true
-
-  cd it
-  mvn -DsonarRunner.version="2.5-SNAPSHOT" -Dsonar.runtimeVersion="5.1.2" -Dmaven.test.redirectTestOutputToFile=false install
-  ;;
-
-IT-DEV-LTS)
-  installTravisTools
-
-  mvn install -Dsource.skip=true -Denforcer.skip=true -Danimal.sniffer.skip=true -Dmaven.test.skip=true
-
-  cd it
-  mvn -DsonarRunner.version="2.5-SNAPSHOT" -Dsonar.runtimeVersion="4.5.5" -Dmaven.test.redirectTestOutputToFile=false install
-  ;;
-
-esac
+fi
