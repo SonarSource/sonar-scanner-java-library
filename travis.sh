b7da5b7de93bbf842530b6c10c0ab5c4d251ddc4
Julien HENRY <julien.henry@sonarsource.com>
2016-04-07 13:31:37 +0200

    Update travis configuration

--- travis.sh
+++ travis.sh
@@ -2,38 +2,23 @@
 
 set -euo pipefail
 
-function installTravisTools {
+function configureTravis {
   mkdir ~/.local
-  curl -sSL https://github.com/SonarSource/travis-utils/tarball/v21 | tar zx --strip-components 1 -C ~/.local
+  curl -sSL https://github.com/SonarSource/travis-utils/tarball/v27 | tar zx --strip-components 1 -C ~/.local
   source ~/.local/bin/install
 }
+configureTravis
 
-if [ -n "${PR_ANALYSIS:-}" ] && [ "${PR_ANALYSIS}" == true ]
-then
-  if [ "$TRAVIS_PULL_REQUEST" != "false" ]
-  then
-    # For security reasons environment variables are not available on the pull requests
-    # coming from outside repositories
-    # http://docs.travis-ci.com/user/pull-requests/#Security-Restrictions-when-testing-Pull-Requests
-    if [ -n "$SONAR_GITHUB_OAUTH" ]; then
+case "$TARGET" in
 
-      # Switch to java 8 as the Dory HTTPS certificate is not supported by Java 7
-      export JAVA_HOME=/usr/lib/jvm/java-8-oracle
-      export PATH=$JAVA_HOME/bin:$PATH
+CI)
+  regular_mvn_build_deploy_analyze
+  ;;
 
-      # PR analysis
-      mvn verify sonar:sonar -B -e -V \
-        -Dsonar.analysis.mode=issues \
-        -Dsonar.github.pullRequest=$TRAVIS_PULL_REQUEST \
-        -Dsonar.github.repository=$TRAVIS_REPO_SLUG \
-        -Dsonar.github.login=$SONAR_GITHUB_LOGIN \
-        -Dsonar.github.oauth=$SONAR_GITHUB_OAUTH \
-        -Dsonar.host.url=$SONAR_HOST_URL \
-        -Dsonar.login=$SONAR_LOGIN \
-        -Dsonar.password=$SONAR_PASSWD  
-    fi
-  fi
-else
-  # Regular CI
-  mvn verify -B -e -V
-fi
+*)
+  echo "Unexpected TARGET value: $TARGET"
+  exit 1
+  ;;
+
+esac
+
