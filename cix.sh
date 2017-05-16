f4f139adc435ab654da556f142b632adfae626ec
Julien HENRY <henryju@yahoo.fr>
2017-05-16 10:56:14 +0200

    Update Orchestrator to latest release

--- cix.sh
+++ cix.sh
@@ -3,4 +3,4 @@ set -euo pipefail
 echo "Running with SQ=$SQ_VERSION JAVA_VERSION=$JAVA_VERSION"
 
 cd its
-mvn verify -Prun-its -Dsonar.runtimeVersion=$SQ_VERSION -DjavaVersion=$JAVA_VERSION
+mvn -B -e verify -Prun-its -Dsonar.runtimeVersion=$SQ_VERSION -DjavaVersion=$JAVA_VERSION
