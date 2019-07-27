6a38b62e5ec9056c0777178f055881d2f801259d
Rick <ricksbrown73@gmail.com>
2019-07-28 07:31:05 +1000

    Run unit tests on community pull requests

--- travis.sh
+++ travis.sh
@@ -7,8 +7,13 @@ function configureTravis {
   curl -sSL https://github.com/SonarSource/travis-utils/tarball/v57 | tar zx --strip-components 1 -C ~/.local
   source ~/.local/bin/install
 }
-configureTravis
+if [[ -n ${TRAVIS_SECURE_ENV_VARS+x} && ${TRAVIS_SECURE_ENV_VARS} == true ]]; then
+  configureTravis
 
-export DEPLOY_PULL_REQUEST=true
+  export DEPLOY_PULL_REQUEST=true
 
-regular_mvn_build_deploy_analyze
+  regular_mvn_build_deploy_analyze
+else
+  echo "Travis secure variables not available"
+  mvn install
+fi
