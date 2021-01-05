9d91cbfd927c93b8d28f515aed5d2f8ffbfba97c
Wouter Admiraal <wouter.admiraal@sonarsource.com>
2021-01-05 08:12:59 +0100

    Update license headers for 2021

--- batch/src/test/java/org/sonarsource/scanner/api/internal/batch/DefaultBatchFactoryTest.java
+++ batch/src/test/java/org/sonarsource/scanner/api/internal/batch/DefaultBatchFactoryTest.java
@@ -1,6 +1,6 @@
 /*
  * SonarQube Scanner API - Batch
- * Copyright (C) 2011-2020 SonarSource SA
+ * Copyright (C) 2011-2021 SonarSource SA
  * mailto:info AT sonarsource DOT com
  *
  * This program is free software; you can redistribute it and/or
