0c20b5946db6126664e40f29e8fcb3f5039590bb
Eric Giffon <eric.giffon@sonarsource.com>
2024-04-26 17:08:57 +0200

    BUILD-4823 Fix code missed during rename

--- lib/src/test/java/org/sonarsource/scanner/lib/internal/TempCleaningTest.java
+++ lib/src/test/java/org/sonarsource/scanner/lib/internal/TempCleaningTest.java
@@ -19,18 +19,17 @@
  */
 package org.sonarsource.scanner.lib.internal;
 
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.mockito.Mockito.mock;
-
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.attribute.FileTime;
-
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.TemporaryFolder;
 import org.sonarsource.scanner.lib.internal.cache.Logger;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.mock;
 
 public class TempCleaningTest {
 
@@ -46,12 +45,12 @@ public class TempCleaningTest {
   @Test
   public void should_clean() throws Exception {
     Path dir = temp.newFolder().toPath();
-    Path oldBatch = dir.resolve("sonar-scanner-api-batch656.jar");
+    Path oldBatch = dir.resolve("sonar-scanner-java-library-batch656.jar");
     Files.write(oldBatch, "foo".getBytes(StandardCharsets.UTF_8));
     FileTime fTime = FileTime.fromMillis(System.currentTimeMillis() - 3 * TempCleaning.ONE_DAY_IN_MILLISECONDS);
     Files.setLastModifiedTime(oldBatch, fTime);
     
-    Path youngBatch = dir.resolve("sonar-scanner-api-batch123.jar");
+    Path youngBatch = dir.resolve("sonar-scanner-java-library-batch123.jar");
     Files.write(youngBatch, "foo".getBytes(StandardCharsets.UTF_8));
 
     Path doNotDelete = dir.resolve("jacoco.txt");
