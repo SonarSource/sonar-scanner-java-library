5e603a1ecccfc478eef3f5a185a3600ec472fcea
Julien HENRY <julien.henry@sonarsource.com>
2024-05-03 16:50:18 +0200

    SCANJLIB-208 Set posix permissions when extracting tar.gz

--- lib/src/test/java/org/sonarsource/scanner/lib/internal/util/CompressionUtilsTest.java
+++ lib/src/test/java/org/sonarsource/scanner/lib/internal/util/CompressionUtilsTest.java
@@ -25,6 +25,15 @@ import java.nio.file.Paths;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.io.TempDir;
 
+import static java.nio.file.attribute.PosixFilePermission.GROUP_EXECUTE;
+import static java.nio.file.attribute.PosixFilePermission.GROUP_READ;
+import static java.nio.file.attribute.PosixFilePermission.GROUP_WRITE;
+import static java.nio.file.attribute.PosixFilePermission.OTHERS_EXECUTE;
+import static java.nio.file.attribute.PosixFilePermission.OTHERS_READ;
+import static java.nio.file.attribute.PosixFilePermission.OTHERS_WRITE;
+import static java.nio.file.attribute.PosixFilePermission.OWNER_EXECUTE;
+import static java.nio.file.attribute.PosixFilePermission.OWNER_READ;
+import static java.nio.file.attribute.PosixFilePermission.OWNER_WRITE;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
@@ -68,5 +77,31 @@ class CompressionUtilsTest {
     var toDir = temp.resolve("dir");
     CompressionUtils.extractTarGz(tar, toDir);
     assertThat(toDir.toFile().list()).hasSize(3);
+  }
+
+  @Test
+  void fileMode_conversion() {
+    assertThat(CompressionUtils.fromFileMode(Integer.parseInt("000", 8))).isEmpty();
+    assertThat(CompressionUtils.fromFileMode(Integer.parseInt("001", 8))).containsExactlyInAnyOrder(OTHERS_EXECUTE);
+    assertThat(CompressionUtils.fromFileMode(Integer.parseInt("002", 8))).containsExactlyInAnyOrder(OTHERS_WRITE);
+    assertThat(CompressionUtils.fromFileMode(Integer.parseInt("003", 8))).containsExactlyInAnyOrder(OTHERS_WRITE, OTHERS_EXECUTE);
+    assertThat(CompressionUtils.fromFileMode(Integer.parseInt("004", 8))).containsExactlyInAnyOrder(OTHERS_READ);
+    assertThat(CompressionUtils.fromFileMode(Integer.parseInt("005", 8))).containsExactlyInAnyOrder(OTHERS_READ, OTHERS_EXECUTE);
+    assertThat(CompressionUtils.fromFileMode(Integer.parseInt("006", 8))).containsExactlyInAnyOrder(OTHERS_READ, OTHERS_WRITE);
+    assertThat(CompressionUtils.fromFileMode(Integer.parseInt("007", 8))).containsExactlyInAnyOrder(OTHERS_READ, OTHERS_WRITE, OTHERS_EXECUTE);
+    assertThat(CompressionUtils.fromFileMode(Integer.parseInt("010", 8))).containsExactlyInAnyOrder(GROUP_EXECUTE);
+    assertThat(CompressionUtils.fromFileMode(Integer.parseInt("020", 8))).containsExactlyInAnyOrder(GROUP_WRITE);
+    assertThat(CompressionUtils.fromFileMode(Integer.parseInt("030", 8))).containsExactlyInAnyOrder(GROUP_WRITE, GROUP_EXECUTE);
+    assertThat(CompressionUtils.fromFileMode(Integer.parseInt("040", 8))).containsExactlyInAnyOrder(GROUP_READ);
+    assertThat(CompressionUtils.fromFileMode(Integer.parseInt("050", 8))).containsExactlyInAnyOrder(GROUP_READ, GROUP_EXECUTE);
+    assertThat(CompressionUtils.fromFileMode(Integer.parseInt("060", 8))).containsExactlyInAnyOrder(GROUP_READ, GROUP_WRITE);
+    assertThat(CompressionUtils.fromFileMode(Integer.parseInt("070", 8))).containsExactlyInAnyOrder(GROUP_READ, GROUP_WRITE, GROUP_EXECUTE);
+    assertThat(CompressionUtils.fromFileMode(Integer.parseInt("100", 8))).containsExactlyInAnyOrder(OWNER_EXECUTE);
+    assertThat(CompressionUtils.fromFileMode(Integer.parseInt("200", 8))).containsExactlyInAnyOrder(OWNER_WRITE);
+    assertThat(CompressionUtils.fromFileMode(Integer.parseInt("300", 8))).containsExactlyInAnyOrder(OWNER_WRITE, OWNER_EXECUTE);
+    assertThat(CompressionUtils.fromFileMode(Integer.parseInt("400", 8))).containsExactlyInAnyOrder(OWNER_READ);
+    assertThat(CompressionUtils.fromFileMode(Integer.parseInt("500", 8))).containsExactlyInAnyOrder(OWNER_READ, OWNER_EXECUTE);
+    assertThat(CompressionUtils.fromFileMode(Integer.parseInt("600", 8))).containsExactlyInAnyOrder(OWNER_READ, OWNER_WRITE);
+    assertThat(CompressionUtils.fromFileMode(Integer.parseInt("700", 8))).containsExactlyInAnyOrder(OWNER_READ, OWNER_WRITE, OWNER_EXECUTE);
   }
 }
