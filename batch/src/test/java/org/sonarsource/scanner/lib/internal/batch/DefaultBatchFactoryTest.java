1c028267f0c7d49cdc49ab7736be678420357ed9
Pierre <pierre.guillot@sonarsource.com>
2025-11-12 10:01:21 +0100

    SCANJLIB-285 Update license header from Sonarsource SA to SonarSource Sàrl

--- batch/src/test/java/org/sonarsource/scanner/lib/internal/batch/DefaultBatchFactoryTest.java
+++ batch/src/test/java/org/sonarsource/scanner/lib/internal/batch/DefaultBatchFactoryTest.java
@@ -1,6 +1,6 @@
 /*
  * SonarScanner Java Library - Batch
- * Copyright (C) 2011-2025 SonarSource SA
+ * Copyright (C) 2011-2025 SonarSource Sàrl
  * mailto:info AT sonarsource DOT com
  *
  * This program is free software; you can redistribute it and/or
--- its/it-simple-scanner/src/main/java/com/sonar/scanner/lib/it/Main.java
+++ its/it-simple-scanner/src/main/java/com/sonar/scanner/lib/it/Main.java
@@ -1,6 +1,6 @@
 /*
  * Simple Scanner for ITs
- * Copyright (C) 2011-2025 SonarSource SA
+ * Copyright (C) 2011-2025 SonarSource Sàrl
  * mailto:info AT sonarsource DOT com
  *
  * This program is free software; you can redistribute it and/or
